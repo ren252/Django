9de3705b1411406b95d86c80d4fc682eff2c8fa8
ren252 <ren.komatsu35@gmail.com>
2022-06-14 01:06:03 +0900

    [add] クラスでの記述

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .forms import HelloForm
 
-def index(request,id ,nickname):
-    result = str(id) + ':' + nickname
-    return HttpResponse(result)
+class HelloView(TemplateView):
+
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'message': 'your data',
+            'form': HelloForm(),
+        }
+
+    def get(self,request):
+        return render(request, 'hello/index.html', self.params)
+
+    def post(self,request):
+        self.params['message'] = request.POST['name'] + ":" + request.POST['mail'] + ":" + request.POST['age']
+        self.params["form"] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
+
